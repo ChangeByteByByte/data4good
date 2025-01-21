3609aaf69b694935500942b84dd6567762c6ced9
slow-connect <louisdeberlin+github@gmail.com>
2025-01-21 22:20:23 +0100

    all working

--- create_socioeconomic_index.py
+++ create_socioeconomic_index.py
@@ -9,50 +9,48 @@ new_cols = [col for col in econ_df.columns if ("2021" in col or not "20" in col)
 
 # %%
 econ_df = econ_df[new_cols]
-econ_df
+econ_df.columns
 
-# %%
-import numpy as np
+# # %%
+# import numpy as np
 
-# %%
-econ_df.drop("2021\r\nLangzeitarbeitslosenquote (%)", axis=1, inplace=True)
+# # %%
+# econ_df.drop("2021\r\nLangzeitarbeitslosenquote (%)", axis=1, inplace=True)
 
-# %%
-for col in econ_df.columns:
-    if "2021" not in col:
-        continue
-    try:
-        # print(econ_df[col].str.replace(",", "."))
-        econ_df[col] = econ_df[col].str.replace(",", ".").astype(float)
-    except:
-        print("column not string")
+# # %%
+# for col in econ_df.columns:
+#     if "2021" not in col:
+#         continue
+#     try:
+#         # print(econ_df[col].str.replace(",", "."))
+#         econ_df[col] = econ_df[col].str.replace(",", ".").astype(float)
+#     except:
+#         print("column not string")
 
-# %%
-#econ_df["2021\r\nAnteil 65- bis 79-Jährige (%)"] = econ_df["2021\r\nAnteil 65- bis 79-Jährige (%)"]
-# TODO: Convert to float!!!
-# econ_df["2021\r\nAnteil 65- bis 79-Jährige (%)"] = econ_df["2021\r\nAnteil 65- bis 79-Jährige (%)"].replace(",", ".")
-# econ_df["2021\r\nAnteil ab 80-Jährige (%)"] = econ_df["2021\r\nAnteil ab 80-Jährige (%)"].replace(",", ".")
-econ_df["2021\r\nAnteil ab 65-Jährige (%)"] = econ_df["2021\r\nAnteil 65- bis 79-Jährige (%)"] + econ_df["2021\r\nAnteil ab 80-Jährige (%)"]
+# # %%
+# #econ_df["2021\r\nAnteil 65- bis 79-Jährige (%)"] = econ_df["2021\r\nAnteil 65- bis 79-Jährige (%)"]
+# # TODO: Convert to float!!!
+# # econ_df["2021\r\nAnteil 65- bis 79-Jährige (%)"] = econ_df["2021\r\nAnteil 65- bis 79-Jährige (%)"].replace(",", ".")
+# # econ_df["2021\r\nAnteil ab 80-Jährige (%)"] = econ_df["2021\r\nAnteil ab 80-Jährige (%)"].replace(",", ".")
+# econ_df["2021\r\nAnteil ab 65-Jährige (%)"] = econ_df["2021\r\nAnteil 65- bis 79-Jährige (%)"] + econ_df["2021\r\nAnteil ab 80-Jährige (%)"]
 
-cols = econ_df.columns
+# cols = econ_df.columns
 
-econ_df = econ_df[list(set(econ_df.columns).difference(set(["2021\r\nAnteil ab 80-Jährige (%)", "2021\r\nAnteil 65- bis 79-Jährige (%)", "2021\r\nKaufkraft (Euro/Haushalt)"])))]
-econ_df
-
-
-# %%
-# normalize the factors
-for col in econ_df.columns:
-    if "2021" not in col:
-        continue
-    econ_df[f"{col}_normalized"] = econ_df[col] / econ_df[col].max()
-
-econ_df["socio_economic_index"] = econ_df["2021\r\nHaushalte mit niedrigem Einkommen (%)_normalized"] + econ_df["2021\r\nAnteil ab 65-Jährige (%)_normalized"] + econ_df["2021\r\nVerschuldung im Kernhaushalt (Euro je Einwohner:in)_normalized"] - econ_df["2021\r\nAllgemeine Deckungsmittel (Euro je Einwohner:in)_normalized"]
-
-# %%
-econ_df["socio_economic_index"]
-
-# %%
-econ_df.to_csv("data/socioeconomic_factors_new.csv")
+# econ_df = econ_df[list(set(econ_df.columns).difference(set(["2021\r\nAnteil ab 80-Jährige (%)", "2021\r\nAnteil 65- bis 79-Jährige (%)", "2021\r\nKaufkraft (Euro/Haushalt)"])))]
+# econ_df
 
 
+# # %%
+# # normalize the factors
+# for col in econ_df.columns:
+#     if "2021" not in col:
+#         continue
+#     econ_df[f"{col}_normalized"] = econ_df[col] / econ_df[col].max()
+
+# econ_df["socio_economic_index"] = econ_df["2021\r\nHaushalte mit niedrigem Einkommen (%)_normalized"] + econ_df["2021\r\nAnteil ab 65-Jährige (%)_normalized"] + econ_df["2021\r\nVerschuldung im Kernhaushalt (Euro je Einwohner:in)_normalized"] - econ_df["2021\r\nAllgemeine Deckungsmittel (Euro je Einwohner:in)_normalized"]
+
+# # %%
+# econ_df["socio_economic_index"]
+
+# # %%
+# econ_df.to_csv("data/socioeconomic_factors_new.csv")
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
+import mapclassify
+
 # from convert_crs import convert_netcdf_to_utm32n as convert_crs
 
 df = pd.read_csv('data/data_morgenpost.csv')
@@ -9,15 +11,17 @@ com = pd.read_csv('data/communen.csv', sep=';')
 
 # print(min(df['ags']), max(df['ags']))
 # print(min(com['GKZ']), max(com['GKZ']))
-
+rlf = 7338
 com['ags'] = com['GKZ']
 com['ags'] = com['ags'] / 1000
 com['ags'] = com['ags'].astype(int)
 
-rename_col = {'HI': 'Heat', 'ET': 'Ice', 'SU': 'HDays', 'TN' : 'HNight', 'DD' : 'DryDays', 'R20mm' : 'Rain'}
+rename_col = {'HI': 'Heat', 'ET': 'Ice', 'SU': 'HDays',
+    'TN': 'HNight', 'DD': 'DryDays', 'R20mm': 'Rain'}
 
 cols = df.columns
-cols = [c for c in cols if ((c[-4:] == 'near' or c[-3:] == 'obs') and '85' in c) or c == 'ags']
+cols = [c for c in cols if (
+    (c[-4:] == 'near' or c[-3:] == 'obs') and '85' in c) or c == 'ags']
 # print(cols)
 df = df[cols]
 near = [c for c in cols if 'near' in c and c != 'ET_rsp85_near']
@@ -27,9 +31,16 @@ for c in near:
     df[c+'_normalised'] = df[c] / df[c].abs().max()
 near = [c+'_normalised' for c in near]
 df['c_score'] = df[near].sum(axis=1)
+df['c_score_normalised'] = (
+    df['c_score'] - min(df['c_score'])) / (max(df['c_score']) - min(df['c_score']))
 
-df_for_map = df[['ags', 'c_score']]
+## codes for Hamburg and Berlin
+berlin = 11000
+hh = 2000
+df[df['GKZ'] == berlin]['socio_economic_index_normalised']
+df[df['GKZ'] == hh]['socio_economic_index_normalised']
 
+df_for_map = df[['ags', 'c_score', 'c_score_normalised']]
 
 
 map = gpd.read_file('data/Shapefiles/shapefile.shp')
@@ -38,26 +49,36 @@ map['ags'] = map['schluessel'].astype(int)
 map = map.merge(df_for_map, on='ags')
 
 
-def makemap(col=None, title=''):
-    map.plot(column=col, figsize=(6, 8), edgecolor="black", legend=True).set_axis_off()
-    plt.title(title)
+def makemap(col=None, title='', cmap=None):
+    map.plot(column=col, figsize=(6, 8),edgecolor="black", legend=False, cmap=cmap, scheme='EqualInterval', k=5).set_axis_off()
+    # plt.title(title)
     # plt.show()
-    plt.savefig(f'img/{title}.svg', transparent=True)
-    plt.savefig(f'img/{title}.png', transparent=True)
+    # plt.savefig(f'img/{title}.svg', transparent=True)
+    # plt.savefig(f'img/{title}.png', transparent=True)
 
 # makemap(col='c_score', title='Climate Impact Indicator')
 
 
 econ = pd.read_csv('data/socioeconomic_factors_new.csv', sep=',')
-econ = econ[['GKZ', 'socio_economic_index']]
-econ['ags'] = econ['GKZ'] // 1000
+econ['socio_economic_index_normalised'] = (econ['socio_economic_index'] - min(econ['socio_economic_index'])) / (max(econ['socio_economic_index']) - min(econ['socio_economic_index']))
+tmp = econ[econ['GKZ'] == berlin*1000]['socio_economic_index_normalised']
+tmp = econ[econ['GKZ'] == hh*1000]['socio_economic_index_normalised']
 
-map = map.merge(econ, on='ags')
+econ=econ[['GKZ', 'socio_economic_index', 'socio_economic_index_normalised']]
+econ['ags']=econ['GKZ'] // 1000
+
+map=map.merge(econ, on='ags')
 # print(map.columns)
-
-corrolation = map['c_score'].corr(map['socio_economic_index'])
-print(corrolation)
-
-makemap(col='socio_economic_index', title='socio')
-makemap(col='c_score', title='climat')
+map['investment'] = map['c_score_normalised'] + map['socio_economic_index_normalised']
+# print(map.loc[map['investment'].idxmax()])
+lst = list(map['investment'])
+lst.sort()
+plt.plot(range(len(lst)), lst, '.')
 # plt.show()
+
+corrolation=map['c_score'].corr(map['socio_economic_index'])
+# print(corrolation)
+
+
+makemap(col='socio_economic_index_normalised', title='socio', cmap='Purples')
+makemap(col='c_score_normalised', title='climat', cmap='Reds' )
